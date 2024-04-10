be3c1cac738d6e8ffd7b105644cda7150da06e80
Sxy <violet.evergarden.sxy@gmail.com>
2024-04-10 12:56:49 +0800

    1

--- Follow/http_tcpServer_linux.h
+++ Follow/http_tcpServer_linux.h
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_HTTP_TCPSERVER_LINUX
 #define INCLUDE_HTTP_TCPSERVER_LINUX
 #include<sys/socket.h>
+#include<arpa/inet.h>
+#include<string>
 namespace http
 {
     class TcpServer
@@ -8,10 +10,21 @@ namespace http
     public:
         TcpServer();
         ~TcpServer();
+    private:
         int startServer();
+        void closeServer();
+        
 
     private:
-     std::string m_ip_address;
+       std::string m_ip_address;
+        int m_port;
+        int m_socket;
+        int m_new_socket;
+        long m_incomingMessage;
+        struct sockaddr_in m_socketAddress;
+        unsigned int m_socketAddress_len;
+        std::string m_serverMessage;
+
     };
 }
 #endif
